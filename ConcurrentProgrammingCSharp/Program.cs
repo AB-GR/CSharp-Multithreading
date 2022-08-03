f961dd48607ad92f9e73f3bfb2d43bc4da8756cf
Abhilash <abhilashgraja@gmail.com>
2022-08-03 12:43:22 +0530

    creating tasks

--- ConcurrentProgrammingCSharp/Program.cs
+++ ConcurrentProgrammingCSharp/Program.cs
@@ -4,7 +4,46 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			//Task.Factory.StartNew(() => Write('1'));
+			//var t = new Task(() => Write('2'));
+			//t.Start();
+			//Write('3');
+
+			//Task.Factory.StartNew(Write, '1');
+			//var t = new Task(Write, 2);
+			//t.Start();
+			//Console.ReadKey();
+
+			var t1 = Task.Factory.StartNew<int>(TextLength, "Testing");
+			var t2 = new Task<int>(TextLength, "New");
+			t2.Start();
+
+			Console.WriteLine($"T1 result is {t1.Result}");
+			Console.WriteLine($"T2 result is {t2.Result}");
+		}
+
+		static int TextLength(object arg)
+		{
+			Console.WriteLine($"The task with id {Task.CurrentId} with arg {arg} is processing..");
+			return arg.ToString().Length;
+		}
+
+		static void Write(object arg)
+		{
+			int i = 1000;
+			while (i --> 0)
+			{
+				Console.Write(arg);
+			}
+		}
+
+		static void Write(char c)
+		{
+			int i = 1000;
+			while(i --> 0)
+			{
+				Console.Write(c);
+			}
 		}
 	}
 }
