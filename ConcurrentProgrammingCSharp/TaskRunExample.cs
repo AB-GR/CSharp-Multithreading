0c7e4a84cc507056978610ddf10eb9cc1e021338
Abhilash <abhilashgraja@gmail.com>
2022-08-23 17:43:01 +0530

    TaskRun Unwrap example

---
+++ ConcurrentProgrammingCSharp/TaskRunExample.cs
@@ -0,0 +1,27 @@
+﻿namespace ConcurrentProgrammingCSharp
+{
+	public class TaskRunExample
+	{
+		public static async void Run()
+		{
+			var unwrappedTask = Task.Factory.StartNew(async delegate {
+				await Task.Delay(1000);
+				return 123;
+			},
+			CancellationToken.None,
+			TaskCreationOptions.DenyChildAttach,
+			TaskScheduler.Default).Unwrap();
+
+			var unwrappedI = await await Task.Factory.StartNew(async delegate {
+				await Task.Delay(1000);
+				return 123;
+			},
+			CancellationToken.None,
+			TaskCreationOptions.DenyChildAttach,
+			TaskScheduler.Default);
+
+			//Task.WaitAll & Task.WaitAny are blocking
+			//Task.WhenAll & Task.WhenAny are awaitable equivalents
+		}
+	}
+}
