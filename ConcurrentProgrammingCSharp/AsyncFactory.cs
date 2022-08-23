6e259068b374b946b56acac1c9a47a6de769161a
Abhilash <abhilashgraja@gmail.com>
2022-08-23 17:48:29 +0530

    async initialization

---
+++ ConcurrentProgrammingCSharp/AsyncFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace ConcurrentProgrammingCSharp
+{
+	public class Foo
+	{
+		private Foo()
+		{
+		}
+
+		private async Task<Foo> InitAsync()
+		{
+			await Task.Delay(1000);
+			return this;
+		}
+
+		public static Task<Foo> CreateFooAsync()
+		{
+			var foo = new Foo();
+			return foo.InitAsync();
+		}
+	}
+
+	public class AsyncFactory
+	{
+		public async Task Run()
+		{
+			var foo = await Foo.CreateFooAsync();
+		}
+	}
+}
